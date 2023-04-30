1a45605d6fcbe17bbc1569b17d6723c9780b299c
vasu <randyvasu0@gmail.com>
2023-04-30 15:38:32 +0530

    2 commit

--- restro/settings.py
+++ restro/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!g2sr5$b%)l6qrmg3_zl)gyit5kr+_58w48puo86+kxdn$dp_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
